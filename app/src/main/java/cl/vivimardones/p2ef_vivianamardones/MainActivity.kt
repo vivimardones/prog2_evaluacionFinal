2f349c9eac7683bb03e8687c3fbdc5f79d2f0b1b
vivimardones <des.viviana.mardones@gmail.com>
2025-01-25 15:43:55 -0300

    Se crea primer commit

---
+++ app/src/main/java/cl/vivimardones/p2ef_vivianamardones/MainActivity.kt
@@ -0,0 +1,47 @@
+package cl.vivimardones.p2ef_vivianamardones
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.vivimardones.p2ef_vivianamardones.ui.theme.P2EF_VivianaMardonesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P2EF_VivianaMardonesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P2EF_VivianaMardonesTheme {
+        Greeting("Android")
+    }
+}
